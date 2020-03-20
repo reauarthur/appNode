b5d9eb8a053e70bc14da29779026653165b04780
Arthur REAU <arthur.reau@crie.i-univ-tlse2.fr>
2020-03-20 17:16:04 +0100

    Add TDPug

---
+++ README.md
@@ -0,0 +1,2 @@
+Le TD de la page 24 est sur la branche master
+Le TD de la page 36 est sur la branche TDPug
--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-var fs = require('fs')
+const fs = require('fs');
+const http = require('http');
+const pug = require('pug');
 
 fs.readFile('data.csv', 'utf8', (err, data) =>{
     if(err){
         console.error(err)
         return
     }
-    console.log(data)
+    console.log(data);
+
+    const liste = data.split('\n');
+    let newdata = {};
+    let user = []
+
+    liste.forEach(function (elt){
+        var res = {};
+        var cells = elt.split(";");
+        res.users = cells[0];
+        res.location = cells[1];
+        user.push(res);
+    });
+
+    newdata.users = user;
+
+    const compiledFunction = pug.compileFile('template.pug');
+    const port = 3000;
+
+
+    const server = http.createServer((req, res) => {
+        const generatedTemplate = compiledFunction({
+            data: newdata
+        });
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(generatedTemplate);
+    })
+
+    server.listen(port, () =>{
+        console.log(`Server launch at port ${port}`);
+    });
 })
